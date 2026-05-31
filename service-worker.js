da801678485363ad611b91ead72a0f4634c41521
Lukescottpeck <lukescottpeck@gmail.com>
2026-05-31 00:57:05 -0600

    v3

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Tichu Scorer service worker
 // Bump CACHE_VERSION whenever you change app.js / index.html to force clients to refresh.
-const CACHE_VERSION = "tichu-v2";
+const CACHE_VERSION = "tichu-v3";
 const ASSETS = [
   "./",
   "./index.html",
